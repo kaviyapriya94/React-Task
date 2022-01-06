892b873d7c0f6f097f138f89970aec6100d8b055
CDS-Dell\Admin <kaviyapriya@mindmade.in>
2022-01-06 11:29:12 +0530

    add local storage

--- src/Components/ProductDetails.js
+++ src/Components/ProductDetails.js
@@ -14,23 +14,22 @@ export default function ProductDetails(){
     console.log(count);
     const {state}=useLocation();
     const dispatch = useDispatch();
-    // const [add,setAdd]=useState(1);
     const handleCount=(e)=>{
         e.preventDefault();
-        // setAdd(add+1);
         const add=count.count+1;
         dispatch({ type: actions.UPDATE_COUNT, payload: add});
     }
     return(
         <div>
             <div className="row justify-content-center">
-                <h1 className="text-primary pb-5 text-center">Product Details</h1>
+                <h1 className="text-primary pb-5 text-center pt-2">Product Details</h1>
                 <div className="col-sm-6">
                     <h3>{state.productTitle}</h3>
                     <h4><FontAwesomeIcon icon={faRupeeSign}/> {state.productPrice}</h4>
                     <p>{state.productDescription}</p>
                     <h5>{state.productCategory}</h5>
                     <h5 className="pstar"> <ReactStars
+                        
                         count={5}
                         value={state.productRate}
                         size={26}
@@ -38,16 +37,15 @@ export default function ProductDetails(){
                         isHalf={true}
                         edit={false}
                         
-                    />({state.productRate})</h5>
+                        />({state.productRate})</h5>
                     <h5 className="pb-3">Count: {state.productCount}</h5>
                     <AddToCart handleCount={handleCount}/>
                 </div>
                 <div className="col-sm-4 pt-3">
                     <img src= {state.productImage} alt="pic" height="300vh" width="80%"/>
-                <div/>
+                </div>
+                </div>
             </div>
-            
-    </div>
-</div>
+        
     )
 }
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,13 @@
 import { createStore } from "redux";
 import rootReducer from "./reducers";
 
-const store = createStore(rootReducer);
+const persistedState = localStorage.getItem('count') 
+                       ? JSON.parse(localStorage.getItem('count'))
+                       : {}
+
+const store = createStore(rootReducer,persistedState);
+store.subscribe(()=>{
+    localStorage.setItem('count', JSON.stringify(store.getState()));
+  })
 
 export default store;
